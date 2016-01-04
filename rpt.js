1592a19e52180d30da89e2887f37b79fb694779a
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-01-05 00:47:38 +0100

    Fixes error handling to create a Node Object when a child package is not exist

--- rpt.js
+++ rpt.js
@@ -98,7 +98,11 @@ function loadNode (logical, physical, cache, cb) {
 
   var realpath
   function thenReadPackageJson (er, real) {
-    if (er) return cb(er)
+    var node
+    if (er) {
+      node = new Node(null, logical, physical, er, cache)
+      return cb(null, node)
+    }
     debug('realpath l=%j p=%j real=%j', dpath(logical), dpath(physical), dpath(real))
     var pj = path.join(real, 'package.json')
     realpath = real
